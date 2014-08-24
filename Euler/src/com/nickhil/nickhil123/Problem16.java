5e2755654735dd6c74d5dade00001d084b51d177
Nickhil Revu <nickhil_r@yahoo.com>
2014-08-24 15:55:48 -0500

    commit 5

--- Euler/src/com/nickhil/nickhil123/Problem16.java
+++ Euler/src/com/nickhil/nickhil123/Problem16.java
@@ -1,20 +1,37 @@
 package com.nickhil.nickhil123;
 
+import java.math.BigInteger;
+
+/* Problem16: 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+ * 
+ * 
+ * ANSWER: 88
+ */
 public class Problem16 {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		long n=(long) Math.pow(2,1000);
+		BigInteger n =BigInteger.valueOf((long) Math.pow( 2,1000 ));
 		System.out.println(n);
 		int sum=0;
-		while(n!=0)
+		/*while(!n.equals(BigInteger.valueOf(0)))
 		{
-			int t= (int) (n%10);
+			long t=  (n.mod(BigInteger.valueOf(10)).longValue());
 			sum+=t;
-			n=n/10;
+			n=n.divide(BigInteger.valueOf(10));
+		}*/
+		long no = n.longValue();
+		while(no!=0)
+		{
+			long t = no%10;
+			sum+=t;
+			no=no/10;
 		}
+			
 		System.out.println(sum);
 	}
 
